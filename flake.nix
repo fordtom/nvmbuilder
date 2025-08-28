3632277d6c2d3192d3bb60b103a0d023d3c2e683
Tom Ford <tfordy63@gmail.com>
2025-08-28 23:39:36 +0200

    adding analyzer

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           overlays = [(import rust-overlay)];
         };
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = ["rust-src" "rustfmt" "clippy"];
+          extensions = ["rust-src" "rustfmt" "clippy" "rust-analyzer"];
         };
 
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
