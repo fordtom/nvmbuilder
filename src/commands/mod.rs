b1822e5f30d56ceae9d125f026177099b81ed36a
Tom Ford <tfordy63@gmail.com>
2025-09-19 01:30:09 +0900

    minor

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,14 +3,11 @@ pub mod generate;
 use crate::args::Args;
 use crate::error::NvmError;
 use crate::variant::DataSheet;
+use rayon::prelude::*;
 
 pub fn build_separate_blocks(args: &Args, data_sheet: &DataSheet) -> Result<(), NvmError> {
-	use rayon::prelude::*;
-
-	args
-		.layout
-		.blocks
-		.par_iter()
-		.try_for_each(|input| generate::build_block_single(input, data_sheet, args))
+    args.layout
+        .blocks
+        .par_iter()
+        .try_for_each(|input| generate::build_block_single(input, data_sheet, args))
 }
-
